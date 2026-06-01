44e34245810e0aa2649343974fbf8be7fabe9b71
El vigilante <cololacalempira5@gmail.com>
2026-06-01 10:16:07 -0600

    Actualizar group-info.js

--- plugins/group-info.js
+++ plugins/group-info.js
@@ -5,17 +5,16 @@ const handler = async (m, { conn }) => {
     let metadata = await conn.groupMetadata(m.chat)
     let pp = await conn.profilePictureUrl(m.chat, 'image').catch(() => 'https://files.catbox.moe/22vhqk.jpeg')
     
-    let texto = `𑁍ࠬܓ ⁾ ㅤׄㅤׅㅤׄ INFO DEL GRUPO ㅤ֢ㅤׄㅤׅ\n\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Nombre:* ${metadata.subject}\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *ID:* ${metadata.id}\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Creador:* ${metadata.owner ? '@' + metadata.owner.split('@')[0] : 'Desconocido'}\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Creado:* ${new Date(metadata.creation * 1000).toLocaleString()}\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Descripción:* ${metadata.desc || 'Sin descripción'}\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Miembros:* ${metadata.participants.length}\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Administradores:* ${metadata.participants.filter(p => p.admin).length}\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Solo admins editan:* ${metadata.restrict ? 'Sí' : 'No'}\n`
-    texto += `ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Solo admins hablan:* ${metadata.announce ? 'Sí' : 'No'}\n\n`
-    texto += `> *YO OFC desarrollado por EL VIGILANTE* ૮(˶ᵔ ᵕ ᵔ˶)ა`
+    let texto = '𑁍ࠬܓ ⁾ ㅤׄㅤׅㅤׄ INFO DEL GRUPO ㅤ֢ㅤׄㅤׅ\n\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Nombre:* ' + metadata.subject + '\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *ID:* ' + metadata.id + '\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Creador:* ' + (metadata.owner ? '@' + metadata.owner.split('@')[0] : 'Desconocido') + '\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Creado:* ' + new Date(metadata.creation * 1000).toLocaleString() + '\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Descripción:* ' + (metadata.desc || 'Sin descripción') + '\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Miembros:* ' + metadata.participants.length + '\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Administradores:* ' + metadata.participants.filter(p => p.admin).length + '\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Solo admins editan:* ' + (metadata.restrict ? 'Sí' : 'No') + '\n'
+    texto += 'ׄㅤ𑁍ࠬܓε(´｡•᎑•`)っ ᜒ *Solo admins hablan:* ' + (metadata.announce ? 'Sí' : 'No') + '\n'
     
     await conn.sendMessage(m.chat, {
       image: { url: pp },
@@ -31,6 +30,6 @@ const handler = async (m, { conn }) => {
 
 handler.help = ['infogrupo']
 handler.tags = ['group']
-handler.command = ['infogrupo']
+handler.command = /^(infogrupo|groupinfo|gcinfo|grupoinfo)$/i
 
 export default handler
